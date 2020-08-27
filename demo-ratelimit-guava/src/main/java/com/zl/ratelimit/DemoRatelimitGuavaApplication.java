3fd85fa6fd9bd59c404a98412d1ec9ab69d03d80
zhuanglei <zhuanglei@wayyue.com>
2020-08-27 16:30:03 +0800

    guava指令桶限流

---
+++ demo-ratelimit-guava/src/main/java/com/zl/ratelimit/DemoRatelimitGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.zl.ratelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRatelimitGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRatelimitGuavaApplication.class, args);
+    }
+
+}
---
+++ demo-ratelimit-guava/src/test/java/com/zl/ratelimit/DemoRatelimitGuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zl.ratelimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRatelimitGuavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
