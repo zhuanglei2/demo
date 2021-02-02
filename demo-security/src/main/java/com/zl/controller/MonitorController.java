ec1a0c5862074cb4251ccd9cf6b83b0d851aff41
zhuanglei <zhuanglei@wayyue.com>
2021-02-02 11:21:36 +0800

    权限拦截

---
+++ demo-security/src/main/java/com/zl/controller/MonitorController.java
@@ -0,0 +1,59 @@
+package com.zl.controller;
+
+import cn.hutool.core.collection.CollUtil;
+import com.zl.common.ApiResponse;
+import com.zl.common.PageResult;
+import com.zl.common.Status;
+import com.zl.expection.SecurityException;
+import com.zl.service.MonitorService;
+import com.zl.util.PageUtil;
+import com.zl.util.SecurityUtil;
+import com.zl.vo.OnlineUser;
+import com.zl.vo.PageCondition;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author zhuangl
+ * @version 1.0
+ * @date 2021/2/2 10:45
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/monitor")
+public class MonitorController {
+    @Autowired
+    private MonitorService monitorService;
+
+    /**
+     * 在线用户列表
+     *
+     * @param pageCondition 分页参数
+     */
+    @GetMapping("/online/user")
+    public ApiResponse onlineUser(PageCondition pageCondition) {
+        PageUtil.checkPageCondition(pageCondition, PageCondition.class);
+        PageResult<OnlineUser> pageResult = monitorService.onlineUser(pageCondition);
+        return ApiResponse.ofSuccess(pageResult);
+    }
+
+    /**
+     * 批量踢出在线用户
+     *
+     * @param names 用户名列表
+     */
+    @DeleteMapping("/online/user/kickout")
+    public ApiResponse kickoutOnlineUser(@RequestBody List<String> names) {
+        if (CollUtil.isEmpty(names)) {
+            throw new SecurityException(Status.PARAM_NOT_NULL);
+        }
+        if (names.contains(SecurityUtil.getCurrentUsername())) {
+            throw new SecurityException(Status.KICKOUT_SELF);
+        }
+        monitorService.kickout(names);
+        return ApiResponse.ofSuccess();
+    }
+}
