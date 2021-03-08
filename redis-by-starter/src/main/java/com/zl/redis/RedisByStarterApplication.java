fbd6d01e412519515ff247fbfbc51fd3377243c1
zhuanglei <zhuanglei@wayyue.com>
2021-03-08 16:41:24 +0800

    redis使用starter

---
+++ redis-by-starter/src/main/java/com/zl/redis/RedisByStarterApplication.java
@@ -0,0 +1,13 @@
+package com.zl.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisByStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisByStarterApplication.class, args);
+    }
+
+}
